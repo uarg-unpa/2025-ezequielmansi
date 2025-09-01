465fb5d32725cbbcf9641e23b330d9d6946afe7e
ezequielmansi <108495682+ezequielmansi@users.noreply.github.com>
2025-09-01 17:41:00 +0000

    Update: Actividad práctica de mascotas.pl e Inicio de peliculas.pl

---
+++ Entrega-ConceptosBasicos/peliculas.pl
@@ -0,0 +1,9 @@
+pelicula(inception,ciencia_ficcion).
+pelicula(forrest_gump,drama).
+pelicula(jurassic_park,aventura).
+
+actor(leonardo_dicaprio,inception).
+actor(tom_hanks,forrest_gump).
+
+director(christopher_nolan,inception).
+director(steven_spielberg,jurassic_park).
